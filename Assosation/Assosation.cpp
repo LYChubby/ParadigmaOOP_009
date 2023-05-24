0d4a6f216382af2c4f7fe0612b84fdbefcc448be
LYChubby <f4uzi345@gmail.com>
2023-05-24 17:04:47 +0700

    Membuat Program Assosiation

--- Assosation/Assosation.cpp
+++ Assosation/Assosation.cpp
@@ -33,7 +33,7 @@ public:
 	}
 
 	void tambahPasien(pasien*);
-	void cetakPasien();
+	void cetakPasien();   
 };
 
 void pasien::tambahDokter(dokter* pDokter) {
@@ -59,4 +59,27 @@ void dokter::cetakPasien() {
 		cout << a->nama << "\n";
 	}
 	cout << endl;
+}
+
+int main() {
+	dokter* varDokter1 = new dokter("dr.Toni");
+	dokter* varDokter2 = new dokter("dr.Tono");
+	pasien* varPasien1= new pasien("Andi");
+	pasien* varPasien2 = new pasien("Lia");
+
+	varDokter1->tambahPasien(varPasien1);
+	varDokter1->tambahPasien(varPasien2);
+	varDokter2->tambahPasien(varPasien1);   
+
+	varDokter1->cetakPasien();
+	varDokter2->cetakPasien();
+	varPasien1->cetakDokter();
+	varPasien2->cetakDokter();
+
+	delete varPasien1;
+	delete varPasien2;
+	delete varDokter1;
+	delete varDokter2;
+
+	return 0;
 }
