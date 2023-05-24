1cb78eff12a1eb7b8c336ceda36d5f9949fd09ee
LYChubby <f4uzi345@gmail.com>
2023-05-24 18:42:33 +0700

    Membuat Class Jantung

---
+++ MainJM/jantung.h
@@ -0,0 +1,16 @@
+#ifndef JANTUNG_H
+#define JANTUNG_H
+
+using namespace std;
+
+class jantung {
+public:
+	jantung() {
+		cout << "Jantung Dihidupkan\n";
+	}
+
+	~jantung() {
+		cout << "Jantung Dimatikan\n";
+	}
+};
+#endif
