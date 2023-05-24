73a52ee9404921d1ee833b0e51e2abfed6a89b09
LYChubby <f4uzi345@gmail.com>
2023-05-24 16:28:29 +0700

    Membuat Class Pelajar

--- ParadigmaOOP/ParadigmaOOP.cpp
+++ ParadigmaOOP/ParadigmaOOP.cpp
@@ -19,3 +19,22 @@ public:
 		return a + b;
 	}
 };
+
+class pelajar : public orang {
+public:
+	string sekolah;
+
+	pelajar(string pNama, string pSekolah) :
+		orang(pNama),
+		sekolah(pSekolah) {
+		cout << "Pelajar Dibuat\n" << endl;
+	}
+
+	~pelajar() {
+		cout << "Pelajar Dihapus\n" << endl;
+	}
+
+	string perkenalan() {
+		return "Hallo, Nama Saya " + nama + " Dari Sekolah " + sekolah + "\n\n";
+	}
+};
