6fcf556406a9a61286cd920590ac3584aa9ffdb0
LYChubby <f4uzi345@gmail.com>
2023-05-24 16:28:57 +0700

    Menjalankan Program Paradigma OOP

--- ParadigmaOOP/ParadigmaOOP.cpp
+++ ParadigmaOOP/ParadigmaOOP.cpp
@@ -38,3 +38,11 @@ public:
 		return "Hallo, Nama Saya " + nama + " Dari Sekolah " + sekolah + "\n\n";
 	}
 };
+
+int main() {
+	pelajar siswa1("Andi Laksono", "SMAN 1 Bantul");
+	cout << siswa1.perkenalan();
+	cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+
+	return 0;
+}
